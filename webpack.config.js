8c6798391942bb1afd11cc2d2f9663fdf5400c0d
miquel-lopez-avila <miquel.lopez@i3television.es>
2024-03-02 16:30:11 +0100

    little changes

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
+      favicon: "./public/favicon.ico",
       filename: "index.html",
     }),
     new MiniCssExtractPlugin(),
