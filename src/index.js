4b107815a487deb04b1972b82e7a5e8edf74dd2f
miquel-lopez-avila <miquel.lopez@i3television.es>
2024-03-02 16:39:42 +0100

    little changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Application from "./application/Application";
-import "./style.css";
+import "style.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
